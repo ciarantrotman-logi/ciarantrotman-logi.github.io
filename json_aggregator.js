810c153f53eb4bca6a21405ba525840704ccd653
Ciaran Trotman <ctrotman@logitech.com>
2023-09-05 14:31:31 +0100

    Add Granular Controls to Aggregator

--- json_aggregator.js
+++ json_aggregator.js
@@ -6,21 +6,38 @@ let key = input.value === ''
 let keyboards = [];
 let keys = [];
 function parse(event){
+    document.getElementById('parameter-selector').style.display = 'block';
     process(Array.from(event.target.files)).then(r => construct());
 }
 async function process(files) {
     for (const file of files) {
         let parsed = JSON.parse(await read(file));
         data.push(parsed);
+        let container = document.getElementById('parameter-keys');
         data.forEach((obj, index) => {
             for (const key in obj) {
                 if (!keys.includes(key) && valid(obj, key)) {
                     keys.push(key);
+                    checkbox(obj, key, container);
                 }
             }
         });
     }
-    createParameterSelector();
+}
+function checkbox(obj, key, container){
+    let div = document.createElement('div');
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = key;
+    checkbox.checked = true;
+    div.appendChild(checkbox);
+    let label = document.createElement('label');
+    label.textContent = key;
+    div.appendChild(label);
+    container.appendChild(div);
+}
+function valid(obj, key){
+    return !isNaN(obj[key]) && obj[key] !== null;
 }
 function construct(){
     data.forEach(file =>{
@@ -33,24 +50,6 @@ function construct(){
             });
         }
     })
-}
-function createParameterSelector(){
-    let container = document.getElementById('parameter-selector');
-    keys.forEach(key =>{
-        let div = document.createElement('div');
-        let checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = key;
-        checkbox.checked = true;
-        div.appendChild(checkbox);
-        let label = document.createElement('label');
-        label.textContent = key;
-        div.appendChild(label);
-        container.appendChild(div);
-    })
-}
-function valid(obj, key){
-    return !isNaN(obj[key]);
 }
 async function read(file){
     return new Promise((resolve, reject) => {
@@ -71,22 +70,32 @@ function aggregate(){
     keyboards.forEach(keyboard =>{
         console.log(`${keyboard.name} has ${keyboard.data.length} instances.`);
         let aggregated = {};
-        aggregated[key] = keyboard.name;
+        aggregated["make"] = keyboard.name;
         keys.forEach(key =>{
             if (document.getElementById(key).checked){
                 let values = [];
                 keyboard.data.forEach(object => {
                     values.push(parseFloat(object[key]));
                 });
-                let mean = values.reduce((acc, val) => acc + val, 0) / values.length;
-                let variance = values.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / values.length;
-                aggregated[`${key}-mean`] = mean;
-                aggregated[`${key}-variance`] = variance;
+                if (mean()) {
+                    aggregated[`${key}-mean`] = values.reduce((acc, val) => acc + val, 0) / values.length;
+                }
+                if (variance()){
+                    aggregated[`${key}-variance`] = values.reduce((acc, val) => acc + Math.pow(val - mean, 2), 0) / values.length;;
+                }
             }
         })
         keyboard.aggregated = aggregated;
     })
     download();
+}
+function mean(){
+    return  document.getElementById('data-selection').value === 'mean' ||
+            document.getElementById('data-selection').value === 'mean-and-variance';
+}
+function variance(){
+    return  document.getElementById('data-selection').value === 'variance' ||
+        document.getElementById('data-selection').value === 'mean-and-variance';
 }
 async function download(){
     const zip = new JSZip();
