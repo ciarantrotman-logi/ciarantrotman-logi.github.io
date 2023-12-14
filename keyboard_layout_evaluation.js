c30bc8d0954ece49513b9aaa76588ea02bb77165
ciarantrotman <ctrotman@logitech.com>
2023-12-14 15:31:19 +0000

    Add User Data Capture

--- keyboard_layout_evaluation.js
+++ keyboard_layout_evaluation.js
@@ -2,23 +2,9 @@
 -----[Variable Declaration]
 */
 let key_order = [
-    // Arrow Keys
-    {key_name: "Left Arrow",    key_code: "ArrowLeft"},
-    {key_name: "Right Arrow",   key_code: "ArrowRight"},
-    {key_name: "Up Arrow",      key_code: "ArrowUp"},
-    {key_name: "Down Arrow",    key_code: "ArrowDown"},
-    // Function Keys
-    // {key_name: "F1",            key_code: "F1"},
-    // {key_name: "F2",            key_code: "F2"},
-    // {key_name: "F3",            key_code: "F3"},
-    // {key_name: "F4",            key_code: "F4"},
-    // {key_name: "F6",            key_code: "F6"},
-    // {key_name: "F7",            key_code: "F7"},
-    // {key_name: "F8",            key_code: "F8"},
-    // {key_name: "F9",            key_code: "F9"},
-    // {key_name: "F10",           key_code: "F10"},
-    // {key_name: "F12",           key_code: "F12"},
-    // Number Row
+    /*
+    Number Row
+    */
     {key_name: "1",             key_code: "Digit1"},
     {key_name: "2",             key_code: "Digit2"},
     {key_name: "3",             key_code: "Digit3"},
@@ -29,20 +15,30 @@ let key_order = [
     {key_name: "8",             key_code: "Digit8"},
     {key_name: "9",             key_code: "Digit9"},
     {key_name: "0",             key_code: "Digit0"},
-    // Common Gaming Keys
-    // {key_name: "Left Shift",    key_code: "ShiftLeft"},
-    // {key_name: "Left Control",  key_code: "ControlLeft"},
-    // {key_name: "Tab",           key_code: "Tab"},
-    // {key_name: "Space",         key_code: "Space"},
-    // {key_name: "W",             key_code: "KeyW"},
-    // {key_name: "A",             key_code: "KeyA"},
-    // {key_name: "S",             key_code: "KeyS"},
-    // {key_name: "D",             key_code: "KeyD"},
-    // {key_name: "Q",             key_code: "KeyQ"},
+    /*
+    Function Keys
+    */
+    {key_name: "F1",            key_code: "F1"},
+    {key_name: "F2",            key_code: "F2"},
+    {key_name: "F3",            key_code: "F3"},
+    {key_name: "F4",            key_code: "F4"},
+    {key_name: "F4",            key_code: "F5"},
+    {key_name: "F6",            key_code: "F6"},
+    {key_name: "F7",            key_code: "F7"},
+    {key_name: "F8",            key_code: "F8"},
+    /*
+    Arrow Keys
+    */
+    {key_name: "Left Arrow",    key_code: "ArrowLeft"},
+    {key_name: "Up Arrow",      key_code: "ArrowUp"},
+    {key_name: "Right Arrow",   key_code: "ArrowRight"},
+    {key_name: "Down Arrow",    key_code: "ArrowDown"},
+    /*
+    D Cluster
+    */
     {key_name: "E",             key_code: "KeyE"},
-    {key_name: "C",             key_code: "KeyC"},
     {key_name: "F",             key_code: "KeyF"},
-    // {key_name: "Escape",        key_code: "Escape"},
+    {key_name: "C",             key_code: "KeyC"},
 ]
 
 let base_key = {key_name: "D", key_code: "KeyD"};
@@ -53,6 +49,7 @@ let evaluation_steps= [];
 let task_index = 0;
 let action_index = 0;
 
+let task_started = false;
 let task_completed = false;
 
 let during_task_data_collection = [];
@@ -61,11 +58,16 @@ let output_data = [];
 let progression_readout= document.getElementById('progress-visualisation');
 let base_key_readout= document.getElementById('base-key');
 let target_key_readout= document.getElementById('target-key');
+
+let input_user_name= document.getElementById('user-name');
+let input_keyboard_model= document.getElementById('keyboard-model');
+let input_keyboard_make= document.getElementById('keyboard-make');
+let input_keyboard_condition= document.getElementById('keyboard-condition');
 /*
 -----[System Events]
 */
 document.addEventListener('keydown', function(event) {
-    if (task_completed) return;
+    if (!task_started || task_completed) return;
     calculate_user_performance(event, validate_user_input(event));
     progress_evaluation_task();
     display_progression_information();
@@ -88,7 +90,7 @@ function validate_user_input(event){
 }
 function calculate_user_performance(event, correct_input){
     during_task_data_collection.push({
-        event: event, 
+        event: event,
         correct_input: correct_input,
         task_type: task_type(),
         action_index : action_index,
@@ -126,13 +128,17 @@ function create_evaluation_steps(){
 -----[Data Processing]
 */
 function calculate_task_performance(){
-    for (let i = 1; i < evaluation_steps.length; i++) {
+    for (let i = 1; i < during_task_data_collection.length; i++) {
         let current_input = during_task_data_collection[i];
         let previous_input = during_task_data_collection[i-1];
         output_data.push({
-            input_target: current_input.event.code,
+            user_name: sessionStorage.getItem('user-name'),
+            keyboard_make: sessionStorage.getItem('keyboard-make'),
+            keyboard_model: sessionStorage.getItem('keyboard-model'),
+            keyboard_condition: sessionStorage.getItem('keyboard-condition'),
+            pressed_key_code: current_input.event.code,
+            previous_key_code: previous_input.event.code,
             input_duration: current_input.event.timeStamp - previous_input.event.timeStamp,
-            previous_input: previous_input.event.code,
             correct_input: current_input.correct_input,
             task_type: current_input.task_type,
             action_index : current_input.task_index,
@@ -165,7 +171,7 @@ function display_current_target_key(){
     target_key_readout.innerHTML = current_task().target.key_name;
 }
 function display_progression_information(){
-    progression_readout.innerHTML = `task is ${task_type()} <br> cycle index is ${cycle_index()} <br> steps in cycle is ${steps_in_cycle()} <br> ${task_index} of ${evaluation_steps.length} total steps done <br> current action index is ${action_index}`;
+    //progression_readout.innerHTML = `${task_completed ? 'completed' : 'ongoing'} <br> task is ${task_type()} <br> cycle index is ${cycle_index()} <br> steps in cycle is ${steps_in_cycle()} <br> ${task_index} of ${evaluation_steps.length} total steps done <br> current action index is ${action_index}`;
 }
 function current_task(){
     return evaluation_steps[task_index];
@@ -181,6 +187,25 @@ function steps_in_cycle() {
 }
 function cycle_index(){
     return Math.floor(task_index / steps_in_cycle());
+}
+function sanitised_string(target){
+    let sanitised = target.replace(/\W+/g, "");
+    return sanitised.toLowerCase();
+}
+/*
+-----[Evaluation Logic Functions]
+*/
+function begin_evaluation(){
+    task_started = true;
+    document.getElementById('introduction').style.display = 'none';
+    document.getElementById('key-location-exercise').style.display = 'block';
+    cache_evaluation_data();
+}
+function cache_evaluation_data(){
+    sessionStorage.setItem('user-name', sanitised_string(document.getElementById('user-name').value));
+    sessionStorage.setItem('keyboard-make', sanitised_string(document.getElementById('keyboard-make').value));
+    sessionStorage.setItem('keyboard-model', sanitised_string(document.getElementById('keyboard-model').value));
+    sessionStorage.setItem('keyboard-condition', sanitised_string(document.getElementById('keyboard-condition').value));
 }
 /*
 -----[Initial Calls]
